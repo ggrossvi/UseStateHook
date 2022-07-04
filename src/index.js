ff9e9b01eec6223bdcb06c768de5f3ce8f981a37
ggrossvi <hello.gloria@gmail.com>
2022-07-03 22:41:40 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+var count = 0;
+
+ReactDOM.render(
+  <div className="container">
+    <h1>0</h1>
+    <button>+</button>
+  </div>,
+  document.getElementById("root")
+);
